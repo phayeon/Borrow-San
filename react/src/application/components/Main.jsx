fda3f90a6861241e0600fbf84b12f79564c14d6b
phayeon <phayeon1222@gmail.com>
2022-12-07 19:41:05 +0900

    12-07 19:41

---
+++ react/src/application/components/Main.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const Main = () => {
+    const rentInfo = () => {
+        alert(
+            "[장소명] 보관함\n상세 위치 정보\n잔여 우산 개수 : [null]")
+    }
+
+    return (
+        <body>
+        <h2>메인화면</h2>
+        <input placeholder="Secrch" name="search"/>
+        <button><Link to="/setmenu">메뉴</Link></button><br/>
+        <button onClick={rentInfo}>☂</button>
+        <button><Link to="/claim">고객센터</Link></button><br/>
+        <button><Link to="/qrscan">Borrow</Link></button>
+        </body>
+    )
+}
+
+export default Main
